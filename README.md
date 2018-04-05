b1d24114959d19374ee4df5e3a93f557fde04c9b
misterd <coat.david@gmail.com>
2018-04-05 17:47:49 +0200

    test

---
+++ README.md
@@ -0,0 +1,11 @@
+# Google
+
+La recherche se lance lorsque l'on clique sur le bouton "Rercherche Google".
+
+Pair programming avec :
+
+eesydo: https://github.com/eesydo
+
+Hug-o: https://github.com/Hug-O/THPJ4
+
+mohammadali-bacha https://github.com/mohammadali-bacha
---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+
+function ShowDiv() {
+    document.getElementById("myDIV").style.display = "";
+    document.getElementById("BB").style.display='none';
+}
+
+window.onload = function() {
+  document.getElementById('btn').onclick = function()
+  {
+    window.location.href = "https://www.google.fr/#q="+document.getElementById("url").value
+  };
+}
+
+setTimeout(function(){ alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
